d31b24ab6f5ad26f1c56048f671a41fc2e0bcb83
EdwarAJA <149855194+EdwarAJA@users.noreply.github.com>
2024-08-27 21:41:17 -0500

    Delete src directory

--- src/main/java/pe/edu/upc/avi_aplicaction/serviceinterfaces/IRolesService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.avi_aplicaction.serviceinterfaces;
-
-import pe.edu.upc.avi_aplicaction.entities.roles;
-
-import java.util.List;
-
-public interface IRolesService {
-    public List<roles> list();
-}
